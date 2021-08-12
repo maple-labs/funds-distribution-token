19d189213576d75ac1d124daffd513ef25ab9770
Lucas Manuel <lucas@maple.finance>
2021-08-12 14:46:11 -0400

    chore: Update main branch for consumption (#5)
    * feat: add config file
    
    * chore: Fix CI and modules (SC-3055) (#1)
    
    * refactor: Imports (#3)
    
    * chore: Take latest main math (#4)
    
    Co-authored-by: Lucas Manuel <lucasmanuel@MacBook-Pro-2.local>
    Co-authored-by: Michael De Luca <35537333+deluca-mike@users.noreply.github.com>

--- contracts/BasicFundsTokenFDT.sol
+++ contracts/BasicFundsTokenFDT.sol
@@ -1,31 +1,30 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.6.11;
 
-import { IERC20, SafeERC20 } from "../../../../lib/openzeppelin-contracts/contracts/token/ERC20/SafeERC20.sol";
-
-import { BasicFDT, SignedSafeMath } from "../../funds-distribution-token/contracts/BasicFDT.sol";
+import { IERC20, SafeERC20 } from "../modules/openzeppelin-contracts/contracts/token/ERC20/SafeERC20.sol";
 
 import { IBasicFundsTokenFDT } from "./interfaces/IBasicFundsTokenFDT.sol";
+
+import { BasicFDT } from "./BasicFDT.sol";
 
 /// @title BasicFundsTokenFDT implements the Basic FDT functionality with a separate Funds Token.
 abstract contract BasicFundsTokenFDT is IBasicFundsTokenFDT, BasicFDT {
 
-    using SafeERC20      for  IERC20;
-    using SignedSafeMath for  int256;
+    using SafeERC20 for IERC20;
 
-    IERC20 public override immutable fundsToken;
+    address public override immutable fundsToken;
 
     uint256 public override fundsTokenBalance;
 
     constructor(string memory name, string memory symbol, address _fundsToken) BasicFDT(name, symbol) public {
-        fundsToken = IERC20(_fundsToken);
+        fundsToken = _fundsToken;
     }
 
     function withdrawFunds() public virtual override(IBasicFundsTokenFDT, BasicFDT) {
         uint256 withdrawableFunds = _prepareWithdraw();
 
         if (withdrawableFunds > uint256(0)) {
-            fundsToken.safeTransfer(msg.sender, withdrawableFunds);
+            IERC20(fundsToken).safeTransfer(msg.sender, withdrawableFunds);
 
             _updateFundsTokenBalance();
         }
@@ -38,7 +37,7 @@ abstract contract BasicFundsTokenFDT is IBasicFundsTokenFDT, BasicFDT {
     function _updateFundsTokenBalance() internal virtual override returns (int256) {
         uint256 _prevFundsTokenBalance = fundsTokenBalance;
 
-        fundsTokenBalance = fundsToken.balanceOf(address(this));
+        fundsTokenBalance = IERC20(fundsToken).balanceOf(address(this));
 
         return int256(fundsTokenBalance).sub(int256(_prevFundsTokenBalance));
     }
--- contracts/interfaces/IBasicFundsTokenFDT.sol
+++ contracts/interfaces/IBasicFundsTokenFDT.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.6.11;
 
-import { IERC20 } from  "../../../../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
 import { IBasicFDT } from "./IBasicFDT.sol";
 
 /// @title BasicFundsTokenFDT implements the Basic FDT functionality with a separate Funds Token.
@@ -11,7 +9,7 @@ interface IBasicFundsTokenFDT is IBasicFDT {
     /**
         @dev The `fundsToken` (dividends).
      */
-    function fundsToken() external view returns (IERC20);
+    function fundsToken() external view returns (address);
 
     /**
         @dev The amount of `fundsToken` currently present and accounted for in this contract.
