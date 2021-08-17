0ec7adca0ae4fc4c1dc83979c62c860fc89b8085
JG Carvalho <jg@jgcarv.com>
2021-08-17 13:10:31 -0300

    feat: added test for basic and extended FDT

---
+++ contracts/test/BasicFDT.t.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import { MapleTest }        from "../../modules/maple-test/contracts/test.sol";
+import { ERC20, SafeMath }  from "../../modules/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+import { BasicFDT }         from "../BasicFDT.sol";
+import { IBasicFDT }        from "../interfaces/IBasicFDT.sol";
+
+contract Account {
+
+    function try_basicFDT_updateFundsRecevied(address fdt) external returns(bool ok) {
+        string memory sig = "updateFundsReceived()";
+        (ok,) = fdt.call(abi.encodeWithSignature(sig));
+    }
+
+    function try_basicFDT_transfer(address fdt, address to, uint256 value) external returns(bool ok) {
+        string memory sig = "transfer(address,uint256)";
+        (ok,) = fdt.call(abi.encodeWithSignature(sig, to, value));
+    }
+
+    function basicFDT_withdrawFunds(address fdt) external {
+        IBasicFDT(fdt).withdrawFunds();
+    }
+}
+
+contract CompleteFDT is BasicFDT {
+
+    uint256 public fundsBalance;
+    uint256 public lastFundsBalance;
+
+    constructor(string memory name, string memory symbol) public BasicFDT(name, symbol) {}
+
+
+    function mint(address to, uint256 amt) public {
+        _mint(to, amt);
+    }
+
+    function burn(address account, uint256 amt) public {
+        _burn(account, amt);
+    }
+    function withdrawFunds() public virtual override {
+        uint256 withdrawableFunds = _prepareWithdraw();
+        fundsBalance = fundsBalance.sub(withdrawableFunds);
+        _updateFundsTokenBalance();
+    }
+
+    function _updateFundsTokenBalance() internal override returns (int256 delta) {
+        delta = int256(fundsBalance - lastFundsBalance);
+        lastFundsBalance = fundsBalance;
+    }
+
+    function increaseFundsReceived(uint256 amount) external{
+        fundsBalance = fundsBalance + amount;
+    }
+
+     function pointsCorrection_(address account) external view returns(int256) {
+        return pointsCorrection[account];
+    } 
+
+    function pointsPerShare_() external view returns(uint256) {
+        return pointsPerShare;
+    }
+
+    function pointsMultiplier_() external view returns(uint256) {
+        return pointsMultiplier;
+    }
+}
+
+contract BasicFDTTest is MapleTest {
+
+    using SafeMath for uint256;
+
+    CompleteFDT   token;
+    Account       account1;
+    Account       account2;
+
+    function setUp() public {
+        token    = new CompleteFDT("CompleteFDT", "FDT");
+        account1 = new Account();
+        account2 = new Account();
+    }
+
+    function test_updateFundsReceived() public {
+        assertEq(token.fundsBalance(), 0);
+        // Mint and transfer some funds token to FundsTokenFDT.
+        token.increaseFundsReceived(10000);
+
+        assertTrue(!account1.try_basicFDT_updateFundsRecevied(address(token)));  // Should fail because total supply is zero
+        
+        token.mint(address(account1), 1000);
+        token.mint(address(account2), 5000);
+
+        assertEq(token.pointsPerShare_(), 0);  // Before the execution of `updateFundsReceived`.
+        
+        uint256 shouldBePointsPerCorrection = token.pointsPerShare_().add(uint256(10000).mul(token.pointsMultiplier_())/ token.totalSupply());
+        assertTrue(account1.try_basicFDT_updateFundsRecevied(address(token)));  // Should pass as total supply is greater than 0.
+        // Funds token balance get updated after the `updateFundsReceived()`.
+        assertEq(token.fundsBalance(),                            10000);
+        assertEq(token.pointsPerShare_(),   shouldBePointsPerCorrection);
+
+        shouldBePointsPerCorrection = token.pointsPerShare_().add(uint256(50000).mul(token.pointsMultiplier_())/ token.totalSupply());
+
+        // Transfer more funds
+        token.increaseFundsReceived(50000);
+        token.updateFundsReceived();
+
+        assertEq(token.fundsBalance(),                       60000);
+        assertEq(token.pointsPerShare_(),   shouldBePointsPerCorrection);
+    }
+
+    function test_mint() public {
+        token.mint(address(account1), 1000);
+
+        assertEq(token.balanceOf(address(account1)), 1000);
+        assertEq(token.pointsCorrection_(address(account1)), 0);
+
+        // Mint and transfer some funds token to token.
+        token.increaseFundsReceived(10000);
+        token.updateFundsReceived();
+
+        // Mint more FDTs
+        int256 newPointsCorrection = token.pointsCorrection_(address(account1)) - int256(token.pointsPerShare_().mul(2000));
+        token.mint(address(account1), 2000);
+        assertEq(token.balanceOf(address(account1)), 3000);
+        assertEq(token.pointsCorrection_(address(account1)), newPointsCorrection);
+    }
+
+    function test_burn() public {
+        token.mint(address(account1), 2000);
+        token.increaseFundsReceived(10000);
+        token.updateFundsReceived();
+        int256 oldPointsCorrection = token.pointsCorrection_(address(account1));
+        int256 newPointsCorrection = oldPointsCorrection + int256(token.pointsPerShare_().mul(100));
+        // Mint and transfer some funds token to token.
+        token.burn(address(account1), 100);
+
+        assertEq(token.balanceOf(address(account1)), 1900);
+        assertEq(token.pointsCorrection_(address(account1)), newPointsCorrection);
+    }
+
+    function test_transfer() public {
+        token.mint(address(account1), 2000);
+        token.increaseFundsReceived(10000);
+        token.updateFundsReceived();
+
+        int256 oldPointsCorrectionFrom = token.pointsCorrection_(address(account1));
+        assertTrue(account1.try_basicFDT_transfer(address(token), address(account2), 500));
+        int256 newPointsCorrectionFrom = token.pointsCorrection_(address(account1));
+
+        int256 delta = newPointsCorrectionFrom - oldPointsCorrectionFrom;
+        assertEq(token.pointsCorrection_(address(account2)), -delta);
+    }
+
+    function test_withdrawFunds() public {
+        token.mint(address(account1), 2000);
+        token.mint(address(account2), 3000);
+        token.increaseFundsReceived(10000);
+        token.updateFundsReceived();
+
+        assertEq(token.withdrawnFundsOf(address(account1)), 0);
+        assertEq(token.withdrawnFundsOf(address(account2)), 0);
+
+        uint256 withdrawableFunds1 = token.withdrawableFundsOf(address(account1));
+        uint256 withdrawableFunds2 = token.withdrawableFundsOf(address(account2));
+        
+        account1.basicFDT_withdrawFunds(address(token));
+        account2.basicFDT_withdrawFunds(address(token));
+
+        assertEq(token.withdrawnFundsOf(address(account1)), withdrawableFunds1);
+        assertEq(token.withdrawnFundsOf(address(account2)), withdrawableFunds2);
+
+        assertEq(token.withdrawableFundsOf(address(account1)), 0);
+        assertEq(token.withdrawableFundsOf(address(account2)), 0);
+    }
+
+}
---
+++ contracts/test/ExtendedFDT.t.sol
@@ -0,0 +1,216 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import { MapleTest }        from "../../modules/maple-test/contracts/test.sol";
+import { ERC20, SafeMath }  from "../../modules/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+import { ExtendedFDT }         from "../ExtendedFDT.sol";
+import { IExtendedFDT }        from "../interfaces/IExtendedFDT.sol";
+
+contract Account {
+
+    function try_ExtendedFDT_updateLossesRecevied(address fdt) external returns(bool ok) {
+        string memory sig = "updateLossesReceived()";
+        (ok,) = fdt.call(abi.encodeWithSignature(sig));
+    }
+
+    function try_ExtendedFDT_transfer(address fdt, address to, uint256 value) external returns(bool ok) {
+        string memory sig = "transfer(address,uint256)";
+        (ok,) = fdt.call(abi.encodeWithSignature(sig, to, value));
+    }
+
+    function basicFDT_withdrawFunds(address fdt) external {
+        IExtendedFDT(fdt).withdrawFunds();
+    }
+
+    function basicFDT_recognizeLosses(address fdt) external {
+        CompleteFDT(fdt).recognizeLosses();
+    }
+}
+
+contract CompleteFDT is ExtendedFDT {
+
+    uint256 public fundsBalance;
+    uint256 public lastFundsBalance;
+
+    uint256 public lossesBalance;
+    uint256 public lastLossesBalance;
+
+    constructor(string memory name, string memory symbol) public ExtendedFDT(name, symbol) {}
+
+
+    function mint(address to, uint256 amt) public {
+        _mint(to, amt);
+    }
+
+    function burn(address account, uint256 amt) public {
+        _burn(account, amt);
+    }
+    
+    function _recognizeLosses() internal override returns (uint256 losses) {
+        losses = _prepareLossesWithdraw();
+
+        lossesBalance = lossesBalance.sub(losses);
+
+        _updateLossesBalance();
+    }
+
+    function recognizeLosses() public {
+        _recognizeLosses();
+    }
+    function _updateFundsTokenBalance() internal override returns (int256 delta) {
+        delta = int256(fundsBalance - lastFundsBalance);
+        lastFundsBalance = fundsBalance;
+    }
+
+    function _updateLossesBalance() internal override returns (int256 delta) {
+        delta = int256(lossesBalance - lastLossesBalance);
+        lastLossesBalance = lossesBalance;
+    }
+
+    function increaseFundsReceived(uint256 amount) external{
+        fundsBalance = fundsBalance + amount;
+    }
+
+    function increaseLossesReceived(uint256 amount) external{
+        lossesBalance = lossesBalance + amount;
+    }
+
+    function pointsCorrection_(address account) external view returns(int256) {
+        return pointsCorrection[account];
+    } 
+
+    function lossesCorrection_(address account) external view returns(int256) {
+        return lossesCorrection[account];
+    } 
+
+
+    function lossesPerShare_() external view returns(uint256) {
+        return lossesPerShare;
+    }
+
+     function pointsPerShare_() external view returns(uint256) {
+        return pointsPerShare;
+    }
+
+    function pointsMultiplier_() external view returns(uint256) {
+        return pointsMultiplier;
+    }
+
+    function updateFundsReceived() public override virtual {
+        int256 newFunds = _updateFundsTokenBalance();
+
+        if (newFunds <= 0) return;
+
+        _distributeFunds(newFunds.toUint256Safe());
+    }
+}
+
+contract ExtendedFDTTest is MapleTest {
+
+    using SafeMath for uint256;
+
+    CompleteFDT   token;
+    Account       account1;
+    Account       account2;
+
+    function setUp() public {
+        token    = new CompleteFDT("CompleteFDT", "FDT");
+        account1 = new Account();
+        account2 = new Account();
+    }
+
+    function test_updateLossesReceived() public {
+        assertEq(token.lossesBalance(), 0);
+        // Mint and transfer some funds token to FundsTokenFDT.
+        token.increaseLossesReceived(10000);
+
+        assertTrue(!account1.try_ExtendedFDT_updateLossesRecevied(address(token)));  // Should fail because total supply is zero
+        
+        token.mint(address(account1), 1000);
+        token.mint(address(account2), 5000);
+
+        assertEq(token.lossesPerShare_(), 0);  // Before the execution of `updateFundsReceived`.
+        
+        uint256 shouldBeLossesPerCorrection = token.lossesPerShare_().add(uint256(10000).mul(token.pointsMultiplier_())/ token.totalSupply());
+        assertTrue(account1.try_ExtendedFDT_updateLossesRecevied(address(token)));  // Should pass as total supply is greater than 0.
+        // Funds token balance get updated after the `updateFundsReceived()`.
+        assertEq(token.lossesBalance(),                            10000);
+        assertEq(token.lossesPerShare_(),   shouldBeLossesPerCorrection);
+
+        shouldBeLossesPerCorrection = token.lossesPerShare_().add(uint256(50000).mul(token.pointsMultiplier_())/ token.totalSupply());
+
+        // Transfer more funds
+        token.increaseLossesReceived(50000);
+        token.updateLossesReceived();
+
+        assertEq(token.lossesBalance(),                       60000);
+        assertEq(token.lossesPerShare_(),   shouldBeLossesPerCorrection);
+    }
+
+    function test_mint() public {
+        token.mint(address(account1), 1000);
+
+        assertEq(token.balanceOf(address(account1)), 1000);
+        assertEq(token.lossesCorrection_(address(account1)), 0);
+
+        // Mint and transfer some funds token to token.
+        token.increaseLossesReceived(10000);
+        token.updateLossesReceived();
+
+        // Mint more FDTs
+        int256 newLossesCorrection = token.lossesCorrection_(address(account1)) - int256(token.lossesPerShare_().mul(2000));
+        token.mint(address(account1), 2000);
+        assertEq(token.balanceOf(address(account1)), 3000);
+        assertEq(token.lossesCorrection_(address(account1)), newLossesCorrection);
+    }
+
+    function test_burn() public {
+        token.mint(address(account1), 2000);
+        token.increaseLossesReceived(10000);
+        token.updateLossesReceived();
+        int256 oldLossesCorrection = token.lossesCorrection_(address(account1));
+        int256 newLossesCorrection = oldLossesCorrection + int256(token.lossesPerShare_().mul(100));
+        // Mint and transfer some funds token to token.
+        token.burn(address(account1), 100);
+
+        assertEq(token.balanceOf(address(account1)), 1900);
+        assertEq(token.lossesCorrection_(address(account1)), newLossesCorrection);
+    }
+
+    function test_transfer() public {
+        token.mint(address(account1), 2000);
+        token.increaseFundsReceived(10000);
+        token.updateFundsReceived();
+
+        int256 oldLossesCorrectionFrom = token.lossesCorrection_(address(account1));
+        assertTrue(account1.try_ExtendedFDT_transfer(address(token), address(account2), 500));
+        int256 newLossesCorrectionFrom = token.lossesCorrection_(address(account1));
+
+        int256 delta = newLossesCorrectionFrom - oldLossesCorrectionFrom;
+        assertEq(token.lossesCorrection_(address(account2)), -delta);
+    }
+
+    function test_recognizeLosses() public {
+        token.mint(address(account1), 2000);
+        token.mint(address(account2), 3000);
+        token.increaseLossesReceived(10000);
+        token.updateLossesReceived();
+
+        assertEq(token.recognizedLossesOf(address(account1)), 0);
+        assertEq(token.recognizedLossesOf(address(account2)), 0);
+
+        uint256 recognizableLossesOf1 = token.recognizableLossesOf(address(account1));
+        uint256 recognizableLossesOf2 = token.recognizableLossesOf(address(account2));
+        
+        account1.basicFDT_recognizeLosses(address(token));
+        account2.basicFDT_recognizeLosses(address(token));
+
+        assertEq(token.recognizedLossesOf(address(account1)), recognizableLossesOf1);
+        assertEq(token.recognizedLossesOf(address(account2)), recognizableLossesOf2);
+
+        assertEq(token.recognizableLossesOf(address(account1)), 0);
+        assertEq(token.recognizableLossesOf(address(account2)), 0);
+    }
+
+}
